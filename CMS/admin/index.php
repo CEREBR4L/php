6e17b568721116b3572b5d226810436e328e1cc2
CEREBR4L <mattmillergraphics@gmail.com>
2017-05-22 00:42:17 +0100

    Spliting comments to un/approved and posted to pushlished/draft on graph

--- CMS/admin/index.php
+++ CMS/admin/index.php
@@ -19,6 +19,24 @@
                         <?php include "includes/admin_widgets.php" ?>
 
                         <div class="row">
+                            <?php
+                                $qry = "SELECT * FROM posts WHERE post_status = 'published'";
+                                $select_published_posts = mysqli_query($connect, $qry);
+                                $published_post_count = mysqli_num_rows($select_published_posts);
+
+                                $qry = "SELECT * FROM posts WHERE post_status = 'draft'";
+                                $select_draft_posts = mysqli_query($connect, $qry);
+                                $draft_post_count = mysqli_num_rows($select_draft_posts);
+
+                                $qry = "SELECT * FROM comments WHERE comment_status = 'approved'";
+                                $select_approved_comments = mysqli_query($connect, $qry);
+                                $approved_comments_count = mysqli_num_rows($select_approved_comments);
+
+                                $qry = "SELECT * FROM comments WHERE comment_status = 'unapproved'";
+                                $select_unapproved_comments = mysqli_query($connect, $qry);
+                                $unapproved_comments_count = mysqli_num_rows($select_unapproved_comments);
+                            ?>
+
                             <script type="text/javascript">
                                 google.charts.load('current', {'packages':['bar']});
                                 google.charts.setOnLoadCallback(drawChart);
@@ -27,10 +45,10 @@
                                     var data = google.visualization.arrayToDataTable([
                                     ['Data', 'Count'],
                                     <?php 
-                                        $element_text = ["Posts", "Comments", "Users", "Categories"];
-                                        $element_values = [$post_count, $comment_count, $user_count, $categories_count];
+                                        $element_text = ["Published Posts", "Draft Posts", "Approved Comments", "Unapproved Comments", "Users", "Categories"];
+                                        $element_values = [$published_post_count, $draft_post_count, $approved_comments_count, $unapproved_comments_count, $user_count, $categories_count];
 
-                                        for($i = 0; $i < 4; $i++){
+                                        for($i = 0; $i < 5; $i++){
                                             echo "['{$element_text[$i]}', {$element_values[$i]}],";
                                         }
                                     ?>
