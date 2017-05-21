48355a37099b226e9cd0f5ac5315cb1cda479a6a
CEREBR4L <mattmillergraphics@gmail.com>
2017-05-21 18:05:55 +0100

    Login qry

--- CMS/includes/login.php
+++ CMS/includes/login.php
@@ -7,7 +7,19 @@
         $username = $_POST['username'];
         $password = $_POST['password'];
         
+        $username = mysqli_real_escape_string($connect, $username);
+        $password = mysqli_real_escape_string($connect, $password);
+
+        $qry = "SELECT * FROM users WHERE user_name = '{$username}'";
+        $find_user = mysqli_query($connect, $qry);
         
+        if(!$find_user){
+            die("Failed to find user: " . mysqli_error($connect));
+        }
+
+        while($row = mysqli_fetch_array($find_user)){
+            echo $user_id = $row['user_id'];
+        }
 
     }
 
